42e9c9fb964e12c5bf586a078f6ab7cb0a355069
Hatsune Miku <999Hatsune@gmail.com>
2016-07-17 19:04:46 +0900

    trap and through glyphContours

--- src/DirectX.jl
+++ src/DirectX.jl
@@ -7,6 +7,7 @@ module DirectX
 import Base
 import Relocator
 import Relocator: _mf
+import WCharUTF8
 
 export connect, close
 export initD3DApp, msgLoop
@@ -17,12 +18,45 @@ const _dlls = [:d3d9, :d3dx9, :d3dxconsole, :freetype, :d3dxfreetype2,
 
 const res_default = (512, 512, "_string.png", "res")
 
-type RenderD3DItemsState
+type VERTEX_GLYPH # in D3DxGlyph.h
+  ppDev::Ptr{Ptr{Void}} # LPDIRECT3DDEVICE9 *
+  ppTexture::Ptr{Ptr{Void}} # LPDIRECT3DTEXTURE9 *
+  pVtxGlyph::Ptr{Void} # LPDIRECT3DVERTEXBUFFER9
+  szGlyph::UInt32 # size_t
+end
+
+type GLYPH_TBL # in D3DxFT2_types.h
+  pIS::Ptr{Void} # RENDERD3DITEMSSTATE * # in dx9adl.h
+  pVG::Ptr{Void} # VERTEX_GLYPH * # in D3DxGlyph.h
+  facename::Ptr{Cchar}
+  utxt::Ptr{Cwchar_t}
+  ratio::Float32
+  angle::Float32
+  reserved0::UInt32
+  reserved1::UInt32
+  mode::UInt32
+  td::UInt32
+  tw::UInt32
+  th::UInt32
+  rct::Ptr{Void} # CUSTOMRECT * # in D3DxFT2_types.h
+  glyphBmp::Ptr{Void} # BOOL (*)(GLYPH_TBL *, FT_Bitmap *, FT_Int, FT_Int)
+  matrix::Ptr{Void} # QUOTANIONIC_MATRIX * # in quotanion.h
+  vtx::Ptr{Void} # GLYPH_VTX * # in D3DxFT2_types.h
+  funcs::Ptr{Void} # FT_Outline_Funcs *
+  glyphContours::Ptr{Void} # BOOL (*)(GLYPH_TBL *)
+end
+
+vg = VERTEX_GLYPH(C_NULL, C_NULL, C_NULL, 0) # re-set later
+gt = GLYPH_TBL(C_NULL, C_NULL, C_NULL, C_NULL, # re-set later
+  Float32(6000.), Float32(25.), 0, 0,
+  0, 1024, 256, 256, C_NULL, C_NULL, C_NULL, C_NULL, C_NULL, C_NULL)
+
+type RenderD3DItemsState # in dx9adl.h
   sysChain::Ptr{Void}
   usrChain::Ptr{Void}
-  stat::UInt32
+  stat::UInt32 # BOOL
   mode::UInt32
-  hWnd::UInt32
+  hWnd::UInt32 # HWND
   reserved0::UInt32
   ppSprite::Ptr{Ptr{Void}} # LPD3DXSPRITE *
   imstring::Ptr{Cchar}
@@ -87,16 +121,42 @@ function renderD3DItems(pIS::Ptr{RenderD3DItemsState})
     #
   else
     if ist.nowTime - ist.prevTime < 5
-      ccall(_mf(:d3dxconsole, :debugout), Void, (Ptr{UInt8}, UInt32, UInt32),
+      ccall(_mf(:d3dxconsole, :debugout), Void, (Ptr{UInt8}, UInt32, UInt32,),
         "renderD3DItems %02d %08X\n", ist.fps, ist.nowTime)
+      t = (75. - 60. * ((ist.nowTime >> 4) % 256) / 256) * pi / 180;
+      gt.pIS = pIS
+      vg.ppDev = _mf(:dx9adl, :g_pDev)
+      vg.ppTexture = C_NULL
+      ccall(_mf(:dx9adl, :ReleaseNil), UInt8, (Ptr{Ptr{Void}},), &vg.pVtxGlyph)
+      vg.szGlyph = 0;
+      gt.pVG = pointer_from_objref(vg)
+      gt.facename = pointer("..\\res\\mikaP.ttf".data)
+      gt.utxt = pointer(WCharUTF8.UTF8toWCS("3\u30422\u3041\u3045"))
+      gt.ratio = Float32(6000.)
+      gt.angle = Float32(25.)
+      gt.reserved1 = gt.reserved0 = 0
+      gt.mode = 4
+      gt.td = 1024
+      gt.tw = 256
+      gt.th = 256
+      gt.rct = C_NULL
+      gt.glyphBmp = C_NULL
+      gt.matrix = C_NULL
+      gt.vtx = C_NULL
+      gt.funcs = C_NULL
+      gt.glyphContours = _mf(:d3dxglyph, :D3DXGLP_GlyphContours)
+      ccall(_mf(:d3dxfreetype2, :D3DXFT2_GlyphOutline), UInt32, (Ptr{Void},),
+        pointer_from_objref(gt))
     end
+    ccall(_mf(:d3dxglyph, :D3DXGLP_DrawGlyph), UInt32, (Ptr{Void},),
+      pointer_from_objref(gt))
   end
   return 1::Cint
 end
 
 function initD3DApp(d9::Dx9adl)
   ccall(_mf(:d3dxconsole, :debugout),
-    Void, (Ptr{UInt8}, Ptr{RenderD3DItemsState}),
+    Void, (Ptr{UInt8}, Ptr{RenderD3DItemsState},),
     "adl_test &d9.istat = %08X\n", &d9.istat)
   hInst = ccall((:GetModuleHandleA, :kernel32), stdcall,
     UInt32, (Ptr{Void},),
@@ -106,7 +166,7 @@ function initD3DApp(d9::Dx9adl)
   appName = "juliaAppDx9ADLtest"
   return ccall(_mf(:dx9adl, :InitD3DApp),
     Cint, (UInt32, UInt32, Ptr{UInt8}, Ptr{UInt8}, Ptr{RenderD3DItemsState},
-      Ptr{Void}, Ptr{Void}, Ptr{Void}),
+      Ptr{Void}, Ptr{Void}, Ptr{Void},),
     hInst, nShow, className, appName, &d9.istat,
     cfunction(initD3DItems, Cint, (Ptr{RenderD3DItemsState},)),
     cfunction(cleanupD3DItems, Cint, (Ptr{RenderD3DItemsState},)),
